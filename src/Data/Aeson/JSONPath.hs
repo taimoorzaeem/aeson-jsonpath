1fbc749ed1752a2271549bcf6cae0c1dfa17cffb
M. Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-12-22 00:08:25 +0500

    feat: implement remaining slice selector and add negative index selection

--- src/Data/Aeson/JSONPath.hs
+++ src/Data/Aeson/JSONPath.hs
@@ -51,7 +51,7 @@ traverseJSPSelectors sels doc = JSON.Array $ V.concat $ map traverse' sels
 traverseJSPSelector :: JSPSelector -> JSON.Value -> V.Vector JSON.Value
 traverseJSPSelector (JSPNameSel key) (JSON.Object obj) = maybe V.empty V.singleton $ KM.lookup (K.fromText key) obj
 traverseJSPSelector (JSPNameSel _) _ = V.empty
-traverseJSPSelector (JSPIndexSel idx) (JSON.Array arr) = maybe V.empty V.singleton $ (V.!?) arr idx
+traverseJSPSelector (JSPIndexSel idx) (JSON.Array arr) = maybe V.empty V.singleton $ (if idx >= 0 then (V.!?) arr idx else (V.!?) arr (idx + V.length arr))
 traverseJSPSelector (JSPIndexSel _) _ = V.empty
 traverseJSPSelector (JSPSliceSel sliceVals) (JSON.Array arr) = traverseJSPSliceSelector sliceVals arr
 traverseJSPSelector (JSPSliceSel _) _ = V.empty
@@ -59,11 +59,35 @@ traverseJSPSelector (JSPWildSel JSPWildcard) doc = V.singleton doc
 
 
 traverseJSPSliceSelector :: (Maybe Int, Maybe Int, Int) -> JSON.Array -> V.Vector JSON.Value
-traverseJSPSliceSelector (Just start, Just end, 1) doc = V.slice (normalize start) ((normalize end)-1) doc
+traverseJSPSliceSelector (start, end, step) doc = getSlice start end step doc
   where
+    -- TODO: Refactor this code to make it more pretty
     len = V.length doc
     normalize i = if i >= 0 then i else len + i
-traverseJSPSliceSelector _ _ = V.empty
+
+    sliceNormalized arr' (n_start, n_end) isStepNeg =
+      let (lower, upper) = if isStepNeg then
+            (min (max n_end (-1)) (len-1), min (max n_start (-1)) (len-1))
+          else
+            (min (max n_start 0) len, min (max n_end 0) len)
+      in V.slice lower ((if isStepNeg then 1 else 0)+upper-lower) arr'
+
+    getSlice _ _ 0 _ = V.empty
+    getSlice (Just st) (Just en) step' arr =
+      filterSlice (sliceNormalized arr (normalize st, normalize en) (step' < 0)) step'
+    getSlice (Just st) Nothing step' arr =
+      filterSlice (sliceNormalized arr (normalize st, len) (step' < 0)) step'
+    getSlice Nothing (Just en) step' arr =
+      filterSlice (sliceNormalized arr (0, normalize en) (step' < 0)) step'
+    getSlice Nothing Nothing step' arr = filterSlice arr step'
+
+    -- trying to avoid a step loop and keeping it "functional"
+    filterSlice slice 1   = slice
+    filterSlice slice (-1) = V.reverse slice
+    filterSlice slice n = if n < 0 then
+          V.ifilter (\i _ -> i `mod` (-1 * n) == 0) $ V.reverse $ V.drop (V.length slice `mod` (-1 * n)) slice
+        else
+          V.ifilter (\i _ -> i `mod` n == 0) slice
 
 emptyJSArray :: JSON.Value
 emptyJSArray = JSON.Array V.empty
--- test/QuerySpec.hs
+++ test/QuerySpec.hs
@@ -90,6 +90,25 @@ books1To3And0And1Doc = JSON.toJSON $ [
       , Book { title = "David Copperfield", author = "Charles Dickens", category = "fiction", price = 12.99 }
     ]
 
+data AlphaList = AList [Text]
+  deriving (Show, Generic)
+instance JSON.ToJSON AlphaList
+
+alphaArr :: JSON.Value
+alphaArr = JSON.toJSON $ AList ["a","b","c","d","e","f","g"]
+
+fgArr :: JSON.Value
+fgArr = JSON.toJSON $ AList ["f","g"]
+
+bdArr :: JSON.Value
+bdArr = JSON.toJSON $ AList ["b","d"]
+
+fdArr :: JSON.Value
+fdArr = JSON.toJSON $ AList ["f","d"]
+
+gfedcbaArr :: JSON.Value
+gfedcbaArr = JSON.toJSON $ AList ["g","f","e","d","c","b","a"]
+
 spec :: Spec
 spec = do
   describe "Run JSPQuery on JSON documents" $ do
@@ -105,11 +124,26 @@ spec = do
     it "returns 0-index book item when query is $.store.books[0]" $
       runJSPQuery "$.store.books[0]" rootDoc `shouldBe` Right books0Doc
 
+    it "returns 0-index book item when query is $.store.books[-4]" $
+      runJSPQuery "$.store.books[-4]" rootDoc `shouldBe` Right books0Doc
+
     it "returns 0,2-index item when query is $.store.books[0,2]" $
       runJSPQuery "$.store.books[0,2]" rootDoc `shouldBe` Right books0And2Doc
 
     it "returns 1To3-index when query is $.store.books[1:3]" $
       runJSPQuery "$.store.books[1:3]" rootDoc `shouldBe` Right books1To3Doc
 
-    it "returns 1To3-index and 0,1-index whe nquery is $.store.books[1:3,0,1]" $
+    it "returns 1To3-index and 0,1-index when query is $.store.books[1:3,0,1]" $
       runJSPQuery "$.store.books[1:3,0,1]" rootDoc `shouldBe` Right books1To3And0And1Doc
+
+    it "returns slice with query $[5:]" $
+      runJSPQuery "$[5:]" alphaArr `shouldBe` Right fgArr
+
+    it "returns slice with query $[1:5:2]" $
+      runJSPQuery "$[1:5:2]" alphaArr `shouldBe` Right bdArr
+
+    it "returns slice with query $[5:1:-2]" $
+      runJSPQuery "$[5:1:-2]" alphaArr `shouldBe` Right fdArr
+
+    it "returns slice with query $[::-1]" $
+      runJSPQuery "$[::-1]" alphaArr `shouldBe` Right gfedcbaArr
